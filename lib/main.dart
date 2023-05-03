a780f0b969a3d63419c9549b2c8e96ed085704ce
Sandra Krupa <sandrakrupax7@gmail.com>
2023-05-03 14:50:16 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
-import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:planner/firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
