5efa0cc0d5d5ebcd982746b81fa8bdcda758104e
Sandra Krupa <sandrakrupax7@gmail.com>
2023-05-03 15:04:02 +0200

    add cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:planner/firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +9,6 @@ void main() async {
   );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
