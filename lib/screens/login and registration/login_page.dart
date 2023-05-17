115c866f2380fcd2ec586e826397e7e4b383e847
Sandra Krupa <sandrakrupax7@gmail.com>
2023-05-17 18:47:32 +0200

    create textfield in login page

--- lib/screens/login and registration/login_page.dart
+++ lib/screens/login and registration/login_page.dart
@@ -15,22 +15,53 @@ class LoginPage extends StatelessWidget {
         Scaffold(
           backgroundColor: Colors.transparent,
           body: SafeArea(
-              child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: SizedBox(
-                  height: 200,
-                  child: Center(
-                    child: Text(
-                      'Log into your account',
-                      style: displayXSbold,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SizedBox(
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        'Log into your account',
+                        style: displayXSbold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          )),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter your email',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 16.0,
+                          vertical: 12.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
