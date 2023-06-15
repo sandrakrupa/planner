c72c9cd72eea45499db7faa42af91317efbc8fba
Sandra Krupa <sandrakrupax7@gmail.com>
2023-06-15 13:01:44 +0200

    add flutter_bloc and create login cubit

---
+++ lib/screens/login and registration/cubit/cubit/login_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubitCubit extends Cubit<LoginCubitState> {
+  LoginCubitCubit() : super(LoginCubitState());
+}
